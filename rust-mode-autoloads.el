80ea13496d3684906404f2c9f114bf1b6a96dd23
michaelbeaumont <mjboamail@gmail.com>
2015-03-29 20:38:44 +0200

    Added autoloading

---
+++ rust-mode-autoloads.el
@@ -0,0 +1,13 @@
+;;; rust-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+(autoload 'rust-mode "rust-mode" "\
+Major mode for Rust code.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+;;; rust-mode-autoloads.el ends here
